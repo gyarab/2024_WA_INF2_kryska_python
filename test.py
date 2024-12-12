f8c87c981021c7e8fa412abda501f8ac422f293e
Jakubkryska <117266492+Jakubkryska@users.noreply.github.com>
2024-12-12 09:32:57 +0100

    fibo

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-
+def fibonacci(n):
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("Nuh uh")
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+if __name__ == "__name__":
+    print(fibonacci(1))
